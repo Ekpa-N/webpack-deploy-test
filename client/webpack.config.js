bc958105cf79230f7c93b3e102ad06f7b7218876
Ekpa <ekpa.ntan@gmail.com>
2022-03-26 16:27:29 +0100

    testing port

---
+++ client/webpack.config.js
@@ -0,0 +1,46 @@
+// require ('')
+const path = require('path')
+
+const port = process.env.PORT;
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: "/",
+    },
+    devServer: {
+        // proxy: {
+        //     "/": {
+        //       target: 'http://localhost:6000',
+        //     },
+        //   }
+        // port: '',
+        port: process.env.PORT,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'},
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    }
+}
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 
 const app = express();
-port = process.env.PORT;
+const port = process.env.PORT;
 
 const testObject = [
     {id: 1, name: "Ekpa"},
@@ -13,10 +13,16 @@ const testObject = [
     {id: 3, name: "Jem"},
 ]
 
+
+app.use(express.static(__dirname + '/client/dist'))
 app.use(express.json())
 app.use(cors())
 
+
 app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/client/dist/index.html')
+})
+app.get('/api/friends', (request, response) => {
     response.json(testObject)
 })
 
